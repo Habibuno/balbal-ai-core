3e664d937a7d96c7739d2639ad3fab3dad3efb9d
Ugolin O. <hello@ugolin-olle.com>
2025-06-14 11:31:54 +0200

    fix(core): commitlint & husky setup

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 export default {
-	'*.{js,jsx,ts,tsx}': ['eslint --fix'],
-	'*.{json,md}': ['prettier --write'],
+	'*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+	'*.{json,md,yml,yaml,html,css,scss}': ['prettier --write'],
 };
